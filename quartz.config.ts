db1d69c6c1913fdb90f48a1de6dcd93c87c797ab
Jasmeet Brar <jasmeet.brar@hotmail.com>
2023-10-07 21:19:51 -0400

    Updated quartz config file

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["Private", "Templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
       typography: {
